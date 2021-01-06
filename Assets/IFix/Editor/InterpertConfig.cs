a90ebb01a668a1ccd72ec434cca23b2235e6bdb9
lds <lds2012@gmail.com>
2021-01-06 22:05:48 +0800

    import

---
+++ Assets/IFix/Editor/InterpertConfig.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using IFix;
+
+[Configure]
+public class InterpertConfig
+{
+    [IFix]
+    static IEnumerable<Type> ToProcess
+    {
+        get
+        {
+            return new List<Type>() {
+                typeof(NewBehaviourScript),
+            };
+        }
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using IFix;
+using IFix.Core;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text = Path.Combine(Application.streamingAssetsPath, "Assembly-CSharp.patch.bytes");
+        bool flag = File.Exists(text);
+        if (flag)
+        {
+            Debug.Log("Load HotFix, patchPath=" + text);
+            PatchManager.Load(new FileStream(text, FileMode.Open), true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect((Screen.width - 200) / 2, 20, 200, 100), "Call  FuncA"))
+        {
+            Debug.Log("Button, Call FuncA, result=" + FuncA());
+        }
+    }
+
+    public string FuncA()
+    {
+        return "Old";
+    }
+}
